17150b327610d42bf0d934fb57d9562d42d0f41e
Vinod Tanwar <125426162+vinitanwar@users.noreply.github.com>
2025-03-20 09:57:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server= http.createServer(app)
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://himanshu.futuretouch.org", 
+    origin: "*", 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], 
 
